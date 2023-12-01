740e9549d0297438919e80a4db2d28cb4dea871c
Fra <96795244+Alexyo21@users.noreply.github.com>
2023-12-01 22:44:16 +0100

    to last commit as luma
    Signed-off-by: Fra <96795244+Alexyo21@users.noreply.github.com>

--- rosalina/source/menu.c
+++ rosalina/source/menu.c
@@ -59,8 +59,8 @@
 
 u32 menuCombo = 0;
 bool isHidInitialized = false;
-bool rosalinaOpen = false;
 u32 mcuFwVersion = 0;
+bool rosalinaOpen = false;
 
 void menuToggleLeds(void)
 {         
@@ -85,15 +85,15 @@ void menuMakeLedDabadeedabada(void)
 
 void ledOffStandby(void)
 {
-    mcuHwcInit();
-    u8 off = 0;
-    u8 Off = 3;
-    MCUHWC_WriteRegister(0x28, &off, 1);
-    MCUHWC_WriteRegister(0x2A, &off, 1);
-    MCUHWC_WriteRegister(0x2B, &off, 1);
-    MCUHWC_WriteRegister(0x2C, &off, 1);
-    MCUHWC_WriteRegister(0x29, &Off, 1);
-    mcuHwcExit();
+     if (configExtra.turnLedsOffStandby)
+     {
+        mcuHwcInit();
+        u8 off = 0;
+        u8 Off = 3;
+        MCUHWC_WriteRegister(0x28, &off, 1);
+        MCUHWC_WriteRegister(0x29, &Off, 1);
+        mcuHwcExit();
+     }
 }
 
 // libctru redefinition:
@@ -354,7 +354,7 @@ void menuThreadMain(void)
 
         if(((scanHeldKeys() & menuCombo) == menuCombo) && !rosalinaOpen && !g_blockMenuOpen)
         {
-            openRosalina();      
+            openRosalina();
         }
         
         // instant reboot combo key
@@ -373,6 +373,7 @@ void menuThreadMain(void)
             mcuHwcExit();  
             botStatus = (result >> 5) & 1; // right shift result to bit 5 ("Bottom screen backlight on") and perform bitwise AND with 1
 
+            svcKernelSetState(0x10000, 2); // unblock gsp
             gspLcdInit();
             if(botStatus)
             {
@@ -383,6 +384,7 @@ void menuThreadMain(void)
                 GSPLCD_PowerOnBacklight(BIT(GSP_SCREEN_BOTTOM));
             }
             gspLcdExit();
+            svcKernelSetState(0x10000, 2); // block gsp again
             while (scanHeldKeys() & (KEY_SELECT | KEY_START));
         }
         
